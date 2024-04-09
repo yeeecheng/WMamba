9a6b177f6a8a7a4f11b8d0bc74543e99f4d6b6fb
Yui-Arthur <arthur80633@gmail.com>
2024-04-09 19:42:19 +0800

    update path variable to kaggle env

--- cp_wmamba_from_root.sh
+++ cp_wmamba_from_root.sh
@@ -1,2 +1,3 @@
 cp ./WMambaBot_3d.py U-Mamba/umamba/nnunetv2/nets/
 cp ./nnUNetTrainerWMambaBot.py U-Mamba/umamba/nnunetv2/training/nnUNetTrainer/
+cp ./paths.py U-Mamba/umamba/nnunetv2/ 
--- cp_wmamba_to_root.sh
+++ cp_wmamba_to_root.sh
@@ -1,2 +1,3 @@
 cp U-Mamba/umamba/nnunetv2/nets/WMambaBot_3d.py ./
 cp U-Mamba/umamba/nnunetv2/training/nnUNetTrainer/nnUNetTrainerWMambaBot.py ./
+cp U-Mamba/umamba/nnunetv2/paths.py ./
