c6d4398147ad5219831b78dfaa50e4e0115b8bf0
Yui-Arthur <arthur80633@gmail.com>
2024-04-09 22:39:28 +0800

    update epoch

--- nnUNetTrainerWMambaBot.py
+++ nnUNetTrainerWMambaBot.py
@@ -12,7 +12,7 @@ class nnUNetTrainerWMambaBot(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
         device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 1
+        self.num_epochs = 5
         print("Total Epochs ", self.num_epochs)
 
     @staticmethod
