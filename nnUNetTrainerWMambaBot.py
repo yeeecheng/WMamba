3bea1dd47adba871b79622be6cc833bcfb640912
Yui-Arthur <arthur80633@gmail.com>
2024-04-17 14:32:41 +0800

    add get epochs from env variable

--- nnUNetTrainerWMambaBot.py
+++ nnUNetTrainerWMambaBot.py
@@ -2,6 +2,7 @@ from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.utilities.plans_handling.plans_handler import ConfigurationManager, PlansManager
 from torch import nn
 import torch
+import os
 from nnunetv2.nets.WMambaBot_3d import get_wmamba_bot_3d_from_plans
 from tqdm import tqdm
 
@@ -12,7 +13,9 @@ class nnUNetTrainerWMambaBot(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
         device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 5
+        self.num_epochs = os.environ.get('training_epochs')
+        if self.num_epochs == None:
+            raise ValueError("Epochs environ variable not define")
         print("Total Epochs ", self.num_epochs)
 
     @staticmethod
--- paths.py
+++ paths.py
@@ -41,27 +41,19 @@ data/
 │   │   ├── dataset.json
 │   ├── ...
 """
-base = join(os.sep.join(__file__.split(os.sep)[:-3]), 'data') 
-# or you can set your own path, e.g., base = '/home/user_name/Documents/U-Mamba/data'
-# nnUNet_raw = join(base, 'nnUNet_raw') # os.environ.get('nnUNet_raw')
-# nnUNet_preprocessed = join(base, 'nnUNet_preprocessed') # os.environ.get('nnUNet_preprocessed')
-# nnUNet_results = join(base, 'nnUNet_results') # os.environ.get('nnUNet_results')
+
+# get from env variable
 nnUNet_raw = os.environ.get('nnUNet_raw')
 nnUNet_preprocessed = os.environ.get('nnUNet_preprocessed')
 nnUNet_results = os.environ.get('nnUNet_results')
 
+# if env variable not set, default nnunet folder
+base = join(os.sep.join(__file__.split(os.sep)[:-3]), 'data') 
 if nnUNet_raw is None:
-    print("nnUNet_raw is not defined and nnU-Net can only be used on data for which preprocessed files "
-          "are already present on your system. nnU-Net cannot be used for experiment planning and preprocessing like "
-          "this. If this is not intended, please read documentation/setting_up_paths.md for information on how to set "
-          "this up properly.")
+    nnUNet_raw = join(base, 'nnUNet_raw') 
 
 if nnUNet_preprocessed is None:
-    print("nnUNet_preprocessed is not defined and nnU-Net can not be used for preprocessing "
-          "or training. If this is not intended, please read documentation/setting_up_paths.md for information on how "
-          "to set this up.")
+    nnUNet_preprocessed = join(base, 'nnUNet_preprocessed') 
 
 if nnUNet_results is None:
-    print("nnUNet_results is not defined and nnU-Net cannot be used for training or "
-          "inference. If this is not intended behavior, please read documentation/setting_up_paths.md for information "
-          "on how to set this up.")
+    nnUNet_results = join(base, 'nnUNet_results') 
